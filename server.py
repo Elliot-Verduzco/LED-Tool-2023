eb557cfbbc017b01dbf00e6fc4cf4d5cb360a0b3
Elliot-Verduzco <elliot@elliotverduzco.com>
2023-12-28 05:07:51 -0800

    added login routes and password hash imports

--- server.py
+++ server.py
@@ -1,18 +1,75 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from bson import ObjectId
 import os
 
 app = Flask(__name__)
+
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # MongoDB connection setup
 mongo_conn_str = os.environ.get('MONGO_CONN_STR')
 client = MongoClient(mongo_conn_str)
 db = client.LightingStoreDB
 
+# User session management setup
+@login_manager.user_loader
+def load_user(user_id):
+    # Assuming user_id is just the stringified ObjectId
+    u = db.users.find_one({'_id': ObjectId(user_id)})
+    if not u:
+        return None
+    return User(u['_id'])
+
+class User(UserMixin):
+    # Assuming that user document has an '_id' field
+    def __init__(self, user_id):
+        self.id = str(user_id)
+
+
 @app.route('/')
 def home():
     return 'Welcome to the Lighting Store API!'
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    # Ensure username is unique and password is not empty
+    if username and password and db.users.find_one({'username': username}) is None:
+        # Hash the user's password
+        hashed_pw = generate_password_hash(password)
+        user_id = db.users.insert_one({'username': username, 'password': hashed_pw}).inserted_id
+        return jsonify({'message': 'User created successfully', 'user_id': str(user_id)}), 201
+    else:
+        return jsonify({'message': 'Username already exists or password is empty'}), 400
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    user = db.users.find_one({'username': username})
+    if user and check_password_hash(user['password'], password):
+        user_obj = User(user['_id'])
+        login_user(user_obj)
+        return jsonify({'message': 'Logged in successfully'}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logged out successfully'}), 200
+
+@app.route('/protected', methods=['GET'])
+@login_required
+def protected():
+    return jsonify({'message': 'This is a protected route.'}), 200
 
 @app.route('/add_order', methods=['POST'])
 def add_order():
